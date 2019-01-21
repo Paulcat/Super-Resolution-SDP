6be9bf8948995d30b9d07e1871ddaa8059b51b3e
pcatala <pcatala@dma.ens.fr>
2019-01-22 00:36:32 +0100

    add some tools

---
+++ toolbox/gencolex.m
@@ -0,0 +1,36 @@
+function [mom_colex] = gencolex(fc)
+%GENCOLEX(fc) Returns moment orders (powers) sorted following the
+%colexicographic order
+%
+%   fc: vector (size d) of maximum orders (in absolute value), in each
+%   dimension.
+%
+%   NOTE: analogous of genpow, which does the same for the graded
+%   lexicographic order. Basically, gencolex generates the set \Om_{2l}.
+
+d = numel(fc);
+
+%msgbox('TODO: lexicographic or colexicograhic changes nothing?')
+
+switch d
+    case 1
+        mom_colex = (-fc:fc)';
+    
+    case 2
+        [k1,k2] = meshgrid(-fc(1):fc(1) ,-fc(2):fc(2));
+        k1 = k1';
+        k2 = k2';
+        mom_colex = [k1(:),k2(:)];
+        
+    case 3
+        [k1,k2,k3] = meshgrid(-fc(1):fc(1), -fc(2):fc(2), -fc(3):fc(3));
+        k1 = permute(k1,[2 1 3]);
+        k2 = permute(k2,[2 1 3]);
+        mom_colex = [k1(:),k2(:),k3(:)];
+        
+    otherwise
+        error('TODO');
+end
+
+end
+

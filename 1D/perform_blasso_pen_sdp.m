13bf79a5b6755d08524b785fa688ca966628f29d
pcatala <pcatala@dma.ens.fr>
2019-08-08 15:24:24 +0200

    add cvx dual implementation

---
+++ 1D/perform_blasso_pen_sdp.m
@@ -0,0 +1,89 @@
+function [R,p] = perform_blasso_pen_sdp(fc,blasso,options)
+%PERFORM_BLASSO_PEN_SDP solve "Toeplitz-penalized" BLASSO using SDP
+%   Solve the following program:
+%
+%   min t + tr(R)/m + 1/(2*la)*||y-Az||^2 + 1/(2*rho)*||R - Toep(R)||^2
+%   s.t.    |R  |z|
+%           |___|_| >= 0
+%           |z* |t|
+%
+%   Inputs: 
+%       fc    - cutoff frequency (R is of size (2*fc+1))
+%       blasso - blasso.y, etc... specifies the quantity composing the
+%       objective
+
+n = 2*fc + 1;
+m = n;
+
+solver = getoptions(options,'solver','primal');
+
+y   = blasso.y;
+la  = blasso.lambda;
+rho = blasso.rho;
+gam = blasso.ga;
+C0  = blasso.f0;
+A   = blasso.A;
+As  = blasso.As;
+
+l = length(y);
+
+cvx_solver SDPT3 % SeDuMi | SDPT3
+cvx_precision high
+
+
+switch solver
+    case 'primal'
+        
+        [X,Y] = meshgrid(1:m);
+        toepl = @(s) double(X-Y==s);
+        
+        cvx_begin sdp %quiet
+        variable R(m+1,m+1) hermitian
+        %variable R1(m,m)    hermitian toeplitz
+        variable z(n)       complex
+        dual variable Q
+        Q : R >= 0
+        %
+        T = zeros(m);
+        for j=-m+1:m-1
+            nj = abs(abs(j) - m); % number of diagonal elements
+            dj = sum(diag(R(1:m,1:m),j)) / nj;
+            T = T + dj * toepl(j);
+        end
+        R(1:n,m+1) == z
+        if la==0
+            error('TODO');
+        else
+            minimize( C0 * ( 1/2*(T(1,1) + R(end,end)) + ...
+                1/2/la  * gam * norm(y - A(z), 'fro')^2 + ...
+                1/2/rho * norm(R(1:m,1:m) - T,'fro')^2 ) );
+        end
+        cvx_end
+        
+        p = 1/la * (y-A(z));
+
+    case 'dual'
+        cvx_begin sdp
+        variable Q(m+1,m+1) hermitian
+        variable p(l) complex
+        dual variable R
+        R : Q >= 0
+        Q(m+1,m+1) == 1;
+        Q(1:n,m+1) == As(p);
+        trace(Q) == 2
+        for j = 1:m-1
+            sum(diag(Q,j)) == Q(m+1-j,m+1);
+        end
+        if la==0
+            warning('check factor gam');
+            maximize( sqrt(gam)*real(p'*y) - rho/2 * norm(Q(1:m,1:m)-1/m*eye(m),'fro')^2 );
+        else
+            warning('check factor gam');
+            maximize( sqrt(gam)*real(p'*y) - la/2*gam*norm(p,'fro')^2 - rho/2 * norm(Q(1:m,1:m)-1/m*eye(m),'fro')^2 );
+        end
+        cvx_end
+end
+
+
+end
+
--- 1D/perform_blasso_sdp.m
+++ 1D/perform_blasso_sdp.m
@@ -1,5 +1,5 @@
-function [R,p,z] = perform_blasso_sdp(fc,blasso,options)
-%PERFORM_BLASSO_SDP solve the sdp-blasso
+function [R,p] = perform_blasso_sdp(fc,blasso,options)
+%PERFORM_BLASSO_SDP solve BLASSO using semidefinite programming
 %   Solve the following program:
 %
 %   min t + tr(R)/m + 1/(2*la)*||y-Az||^2
@@ -27,18 +27,24 @@ la  = blasso.lambda;
 gam = blasso.ga;
 C0  = blasso.f0;
 A   = blasso.A;
+As  = blasso.As;
+
+l = length(y);
+
+Am = blasso.Amat;
+Asm = blasso.Asmat;
+B = inv(Asm*Am);
+Ap = pinv(Am);
 
 
 cvx_solver    SDPT3 % SDPT3 | SeDuMi | mosek
 cvx_precision high
 
-
-
 %L = chol(inv(real(AS*A))); % real = HACK?
 
 switch(solver)
     case 'primal'
-        cvx_begin sdp quiet
+        cvx_begin sdp %quiet
         variable R(m+1,m+1) hermitian;
         variable R1(m,m)    hermitian toeplitz;
         variable z(n)       complex;
@@ -56,38 +62,47 @@ switch(solver)
         
         p = 1/la * (y - A(z)); % no AS*y, because PhiS is well implemented ??
         
-    case 'cvx-dual' %TODO!!!!
+    case 'dual' %TODO!!!!
+        
+        %[X,Y] = meshgrid(1:m);
+        %toepl = @(s) double(X-Y==s);
+        
         cvx_begin sdp %quiet
         variable Q(m+1,m+1) hermitian;
-        variable Q1(m,m) hermitian;
-        variable p(n) complex;
+        %variable Q1(m,m) hermitian;
+        variable p(l) complex;
         dual variable R;
         R : Q >= 0;
         Q(m+1,m+1) == 1;
-        Q(1:n,m+1) == p .* conj(w);
-        Q(1:m,1:m) == Q1;
-        Q(n+1:m,m+1) == 0;
-        %         trace(Q) == 2;
-        %         for j = 1:m-1,
-        %             sum(diag(Q,j)) == Q(m+1-j,m+1);
-        %         end
-        for j=0:m-1
-            T = topl(j);
-            T(:)' * Q1(:) == double( j==0 );
+        Q(1:n,m+1) == As(p); %p .* conj(w);
+        %Q(1:m,1:m) == Q1;
+        %Q(n+1:m,m+1) == 0;
+        trace(Q) == 2;
+        for j = 1:m-1,
+            sum(diag(Q,j)) == Q(m+1-j,m+1);
         end
-        if lambda==0
-            maximize(real( p' * y ))
+%         for j=0:m-1
+%             T = toepl(j);
+%             T(:)' * Q1(:) == double( j==0 );
+%         end
+        if la==0
+            warning('check factor gam')
+            maximize( sqrt(gam) * real( p' * y )) % todo: remove...
         else
-            %maximize( real(p'*y) - lambda/2 * norm(p,'fro')^2 );
+            warning('check factor gam')
+            maximize( sqrt(gam) * real(p'*y) - la/2 * gam *norm(p,'fro')^2 );
             %minimize( 1/2*norm(y/lambda-p,'fro')^2 );
             %minimize( lambda/2*norm(y/lambda-p,'fro')^2 - lambda/2*norm(y/lambda,'fro')^2);
-            minimize( 1/2*sqrt(ga)*norm(L*A'*y/lambda - L*p)^2,'fro');
+            %minimize( 1/2*sqrt(ga)*norm(L*A'*y/lambda - L*p)^2,'fro');
         end
         cvx_end
         
-        p = A*p;
+        
+        %p = A*p;
         
 
+
+end
 
 end
 

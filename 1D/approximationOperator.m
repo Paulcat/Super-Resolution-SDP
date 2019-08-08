758abecb18683a3b632efb86978b9e9f1e6405ad
pcatala <pcatala@dma.ens.fr>
2019-08-08 15:16:23 +0200

    cleaner implementation of approximation operator. Add alternative foveation kernel option.

--- 1D/approximationOperator.m
+++ 1D/approximationOperator.m
@@ -1,4 +1,4 @@
-function [A,AS,Amat,ASmat] = approximationOperator(fc,model)
+function [A,As,S,Amat,Asmat] = approximationOperator(fc,model)
 %APPROXIMATIONOPERATOR Compute the Fourier approximation matrix A
 %   [A,AS] = APPROXIMATIONOPERATOR(fc,model) returns the approximation
 %   operator A (and its adjoint AS) of the forward operator specified in
@@ -18,34 +18,54 @@ function [A,AS,Amat,ASmat] = approximationOperator(fc,model)
 %   off-the-grid sparse super-resolution
 
 % n = 2*fc + 1;
-Xf = FreqMesh(fc);
+%Xf = FreqMesh(fc);
+Xf = -fc:fc;
+
+kernel = model.kernel;
 
 switch model.fop
     case 'convolution'
-        w = compute_kernel_weights(fc,model);
+        w = compute_weights(fc,kernel);
         
         A  = @(z) w       .* z;
-        AS = @(p) conj(w) .* p;
+        As = @(p) conj(w) .* p;
+        
+        S = @(z) abs(w).^2 .* z;
         
         Amat  = diag(w);
-        ASmat = Amat';
+        Asmat = Amat';
         
-    case 'subsampled-convolution'
-        w  = compute_kernel_weights  (fc,model);
-        Xg = compute_subsampling_grid(fc,model);
-        L  = length(Xg);
+    case 's-convolution'
+        grid  = model.grid;
+        
+        w  = compute_weights(fc,kernel);
+        Xg = compute_grid(fc,grid);
+        L  = getoptions(grid,'size',64);
+        
+        C = sqrt(1/L);
+        
+        if 2*fc >= L
+            error('grid is insufficiently large wrt fc (TODO)');
+        end
         
         Amat  = exp( 2i*pi * Xg(:) * Xf(:)' ) * diag(w(:));
-        ASmat = sqrt(1/L) * Amat';
+        Asmat = C * Amat';
+        Smat = Asmat*Amat;
         
-        A  = @(z) Amat * ( w(:) .* z(:) );
-        AS = @(p) conj(w(:)) .* (ASmat * p(:));
+        %A  = @(z) Amat * ( w(:) .* z(:) );
+        A  = @(z) exp(2i*pi * Xg(:) * Xf(:)') * (w.*z);
+        %As = @(p) conj(w) .* (Asmat * p(:));
+        As = @(p) (Asmat * p(:));
+        
+        S = @(z) Smat * z(:);
         
     case 'foveation'
-        Xg  = compute_subsampling_grid(fc,model);
+        grid = model.grid;
+        
+        Xg  = compute_grid(fc,grid);
         L   = length(Xg);
         
-        sig = getoptions(model, 'kparam', @(x)(0.001 + .05*abs(x)));
+        sig = getoptions(kernel, 'cov', @(x)(.001 + .05*abs(x)));
         
         N = 2048;
         t = (0:(N-1))'/N;
@@ -69,20 +89,32 @@ switch model.fop
 %             Amat(j,:) = conj(Fj);
 %         end
 % ----------------------
-        
-        phij  = @(X,Xg) exp( -(Xg - X).^2 ./ (2*sig(X).^2) );
-        phij1 = @(X,Xg) phij(X-1,Xg) + phij(X,Xg) + phij(X+1,Xg);
-        
-        [X,G] = meshgrid(t,Xg);
-        B     = phij1(X,G);
-        F     = 1/N * fftshift(fft(B,[],2),2);
-        Fc    = F( : , (N/2 + 1 - fc) : (N/2 + 1 + fc) );
-        
-        Amat  = conj(Fc);
-        ASmat = sqrt(1/L) * Amat';
+
+        switch kernel.mode
+            case 1
+                phij  = @(x,s) exp( -(s - x).^2 ./ (2*sig(x).^2) );
+                phij1 = @(x,s) phij(x-1,s) + phij(x,s) + phij(x+1,s);
+
+                [X,G] = meshgrid(t,Xg);
+                B     = phij1(X,G);
+                F     = 1/N * fftshift(fft(B,[],2),2);
+                Fc    = F( : , (N/2 + 1 - fc) : (N/2 + 1 + fc) );
+
+                Amat  = conj(Fc);
+                Asmat = sqrt(1/L) * Amat';
+
+            case 2
+                em = sqrt(2*pi) * sig(Xg(:)) .* exp(-2i*pi*Xg(:)*Xf(:)');
+                Amat = em .* exp( -2*pi^2*sig(Xg(:)).^2 .* Xf(:)'.^2 );
+                Amat = conj(Amat);
+                Asmat = sqrt(1/L) * Amat';
+        end
         
         A  = @(z) Amat  * z(:);
-        AS = @(p) ASmat * p(:);
+        As = @(p) Asmat * p(:);
+        
+        Smat = Asmat * Amat;
+        S = @(z) Smat * z(:);
             
     otherwise
         error('TODO');
@@ -92,19 +124,20 @@ end
 end
 
 
-function [weights] = compute_kernel_weights(fc,model)
+function [weights] = compute_weights(fc,kernel)
 %COMPUTE_KERNEL_WEIGHTS compute transfer function of filter
 
 d = numel(fc);
 
-switch model.kernel
+switch kernel.type
     case 'Dirichlet'
         n = 2*fc+1;
         weights = ones(n, 1) / n; % normalization l1
+        %weights = ones(n,1);
         
     case 'Gaussian'
         % Fourier coefficients of Gaussian are computed in closed-form
-        sig = getoptions(model, 'kparam', .05);
+        sig = getoptions(kernel, 'cov', .05);
         
         Xf      = FreqMesh(fc);
         weights = ( sqrt(2*pi) * sig )^d * exp( -2*pi^2*sig^2 * Xf(:)'.^2 )';
@@ -116,18 +149,19 @@ end
 end
 
 
-function [Xg] = compute_subsampling_grid(fc,model)
+function [Xg] = compute_grid(fc,grid)
 %COMPUTE_SUBSAMPLING_GRID 
 
-switch model.grid
+switch grid.shape
     case 'lattice'
-        L = getoptions(model, 'gsize', 64);
+        L = getoptions(grid, 'size', 64);
         
         q  = ceil( (2*fc+1) / L );
         t  = 1; %TODO
         Lq = L*q;
         %Xg = linspace(0, (Lq-1)*t, Lq) / Lq;
         Xg = linspace(0, (Lq-1)*t, L) / Lq;
+        Xg = linspace(0, L-1, L) / L;
         
     otherwise
         error('TODO');
